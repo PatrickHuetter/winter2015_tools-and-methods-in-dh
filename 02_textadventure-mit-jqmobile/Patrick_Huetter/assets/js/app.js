54a3a1c30f08718e7ff63f717e672f406605bc7c
Patrick Hütter <p.huetter@encircle360.com>
2016-02-11 23:07:25 +0100

    first working revision of one billion dollars game

---
+++ 02_textadventure-mit-jqmobile/Patrick_Huetter/assets/js/app.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+
+var start = $('#start');
+var fail = $('#fail');
+var success = $('#success');
+var easteregg = $('#lamborghini');
+var a = $('#a');
+var b = $('#b');
+var c = $('#c');
+var d = $('#d');
+
+  // register onclick handler
+  $('#btnStart').click(function() {
+      start.hide();
+      a.show();
+  });
+
+  $('#btnNoEnglish').click(function() {
+    start.hide();
+    fail.show();
+  });
+
+  $('#btnFacebook').click(function() {
+    a.hide();
+    fail.show();
+  });
+
+  $('#btnPlan').click(function(){
+    a.hide();
+    b.show();
+  });
+
+  $('#btnCodeYes').click(function(){
+    b.hide();
+    c.show();
+  });
+
+  $('#btnCodeNo').click(function(){
+    b.hide();
+    fail.show();
+  });
+
+  $('#btnCoFounder').click(function(){
+    c.hide();
+    d.show();
+  });
+
+  $('#btnCoFounderWithoutShare').click(function(){
+    c.hide();
+    fail.show();
+  });
+
+  $('#btnMarketing').click(function(){
+    d.hide();
+    success.show();
+  });
+
+  $('#btnAudi').click(function(){
+    d.hide();
+    fail.show();
+  });
+
+  $('#btnLamborghini').click(function(){
+    success.hide();
+    easteregg.show();
+  });
+});
