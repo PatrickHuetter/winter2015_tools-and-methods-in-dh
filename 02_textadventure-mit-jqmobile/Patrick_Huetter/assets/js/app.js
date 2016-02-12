7a4d796b213951d68cb247c76a1c06dfc66b26bd
Patrick Hütter <p.huetter@encircle360.com>
2016-02-12 16:32:41 +0100

    cleanup

--- 02_textadventure-mit-jqmobile/Patrick_Huetter/assets/js/app.js
+++ 02_textadventure-mit-jqmobile/Patrick_Huetter/assets/js/app.js
@@ -1,78 +1,78 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var start = $('#start');
-var fail = $('#fail');
-var success = $('#success');
-var easteregg = $('#lamborghini');
-var a = $('#a');
-var b = $('#b');
-var c = $('#c');
-var cSpecial = $('#cSpecial');
-var d = $('#d');
+    var start = $('#start');
+    var fail = $('#fail');
+    var success = $('#success');
+    var easteregg = $('#lamborghini');
+    var a = $('#a');
+    var b = $('#b');
+    var c = $('#c');
+    var cSpecial = $('#cSpecial');
+    var d = $('#d');
 
-  // register onclick handler
-  $('#btnStart').click(function() {
-      start.hide();
-      a.show();
-  });
+    // register onclick handler
+    $('#btnStart').click(function () {
+        start.hide();
+        a.show();
+    });
 
-  $('#btnNoEnglish').click(function() {
-    start.hide();
-    fail.show();
-  });
+    $('#btnNoEnglish').click(function () {
+        start.hide();
+        fail.show();
+    });
 
-  $('#btnFacebook').click(function() {
-    a.hide();
-    fail.show();
-  });
+    $('#btnFacebook').click(function () {
+        a.hide();
+        fail.show();
+    });
 
-  $('#btnPlan').click(function(){
-    a.hide();
-    b.show();
-  });
+    $('#btnPlan').click(function () {
+        a.hide();
+        b.show();
+    });
 
-  $('#btnCodeYes').click(function(){
-    b.hide();
-    c.show();
-  });
+    $('#btnCodeYes').click(function () {
+        b.hide();
+        c.show();
+    });
 
-  $('#btnCodeNo').click(function(){
-    b.hide();
-    cSpecial.show();
-  });
+    $('#btnCodeNo').click(function () {
+        b.hide();
+        cSpecial.show();
+    });
 
-  $('#btnCoFounder').click(function(){
-    c.hide();
-    d.show();
-  });
+    $('#btnCoFounder').click(function () {
+        c.hide();
+        d.show();
+    });
 
-  $('#btnCoFounderWithoutShare').click(function(){
-    c.hide();
-    fail.show();
-  });
+    $('#btnCoFounderWithoutShare').click(function () {
+        c.hide();
+        fail.show();
+    });
 
-  $('#btnCoFounderS').click(function(){
-    cSpecial.hide();
-    d.show();
-  });
+    $('#btnCoFounderS').click(function () {
+        cSpecial.hide();
+        d.show();
+    });
 
-  $('#btnCoFounderWithoutShareS').click(function(){
-    cSpecial.hide();
-    fail.show();
-  });
+    $('#btnCoFounderWithoutShareS').click(function () {
+        cSpecial.hide();
+        fail.show();
+    });
 
-  $('#btnMarketing').click(function(){
-    d.hide();
-    success.show();
-  });
+    $('#btnMarketing').click(function () {
+        d.hide();
+        success.show();
+    });
 
-  $('#btnAudi').click(function(){
-    d.hide();
-    fail.show();
-  });
+    $('#btnAudi').click(function () {
+        d.hide();
+        fail.show();
+    });
 
-  $('#btnLamborghini').click(function(){
-    success.hide();
-    easteregg.show();
-  });
+    $('#btnLamborghini').click(function () {
+        success.hide();
+        easteregg.show();
+    });
 });
